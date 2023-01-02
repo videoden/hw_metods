6eaed30e2f04431c00778cc267b55d7f946d0837
qmar <video4den@gmail.com>
2023-01-02 12:19:21 +0500

    создал ветку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
+        //task3();
+
     }
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
 }
